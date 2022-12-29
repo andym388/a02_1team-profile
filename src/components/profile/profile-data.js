0f83a920bc33b36d5069bab781b11bc9be447da7
andym388 <andym388@hotmail.com>
2022-12-30 00:59:45 +1100

    first commit

---
+++ src/components/profile/profile-data.js
@@ -0,0 +1,32 @@
+export const profileData = [
+  {
+    id: 1,
+    name: "Stella B",
+    job: "Developer Advocate",
+    img: "https://i.ibb.co/kXjQ7bH/Portrait-of-a-happy-young-woman-at-home.png",
+  },
+  {
+    id: 2,
+    name: "Alex Jones",
+    job: "UI/UX Designer",
+    img: "https://www.seekpng.com/png/detail/975-9753787_profile-picture-of-alex-jones-profil-picture-john.png",
+  },
+  {
+    id: 3,
+    name: "Sarah K",
+    job: "FrontEnd Developer",
+    img: "https://image.shutterstock.com/image-photo/closeup-photo-amazing-short-hairdo-260nw-1617540484.jpg",
+  },
+  {
+    id: 4,
+    name: "Zino A",
+    job: "JavaScript Developer",
+    img: "https://image.shutterstock.com/image-photo/portrait-happy-businessman-arms-crossed-260nw-388786822.jpg",
+  },
+  {
+    id: 5,
+    name: "Caroline A",
+    job: "Fullstack Developer",
+    img: "https://image.shutterstock.com/image-photo/portrait-attractive-cheerful-girl-demonstrating-260nw-2113649489.jpg",
+  },
+];
